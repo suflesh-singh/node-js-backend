575c90230dbb13a091befd9d3cc04b332aa20d57
suflesh.singh@thrymr.net-at-175029991794 <suflesh.singh@thrymr.net-at-175029991794>
2023-08-17 16:28:07 +0530

    worked on post api

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express")
 const app = express();
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 const ContactUs = require("./models/contactUs")
 
-
+app.use(express.json());
 const cors = require("cors");
 
 const port = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.json());
+
 
 const myProfileData = require("./profileData.json");
-
-const ContactUsQuery=require("./models/contactUs.json")
 
 
 app.get('/', (req, res) => {
@@ -23,40 +21,30 @@ app.get('/my-profile', (req, res) => {
     res.send(myProfileData);
 })
 
-app.get('/my-profile', (req, res) => {
-    res.send(myProfileData);
-})
-
-app.post('/Contact-us', (req, res) => {
-    res.send(ContactUsQuery);
-})
-
-
-
 // contact us post Api htart here
-// app.post('/ContactUs', async (req, res) => {
-//     try {
-//         const Contact = await ContactUs.create(req.body)
-//         res.status(200).json(Contact)
+app.post('/ContactUs', async (req, res) => {
+    try {
+        const Contact = await ContactUs.create(req.body)
+        res.status(200).json(Contact)
 
-//     } catch (error) {
-//         console.log(error.message);
-//         res.status(500).json({ message: error.message })
-//     }
-// })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message })
+    }
+})
 
 
 app.listen(port, () => {
     console.log("hello i am again live")
 });
 
-// mongoose.set("strictQuery", false)
-// mongoose.connect('mongodb+srv://sufleshs:Sufi1212@cluster0.em25nzh.mongodb.net/Node-Api?retryWrites=true&w=majority').then(() => {
-//     app.listen(3000, () => {
-//         console.log('Node Api is running on post 3000')
-//     })
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb+srv://sufleshs:Sufi1212@cluster0.em25nzh.mongodb.net/Node-Api?retryWrites=true&w=majority').then(() => {
+    app.listen(port, () => {
+        console.log('Node Api is running on post 3000')
+    })
 
-// }).catch()
-// {
+}).catch()
+{
 
-// }
+}
